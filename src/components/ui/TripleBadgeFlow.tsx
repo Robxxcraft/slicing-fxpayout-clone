d756d4167bb067171331345c4079be33c692469e
adib <adibhaidarzaky@gmail.com>
2026-02-06 13:06:24 +0700

    step badge flow

--- src/components/ui/TripleBadgeFlow.tsx
+++ src/components/ui/TripleBadgeFlow.tsx
@@ -1,11 +1,11 @@
+import { FaCheck } from "react-icons/fa6";
+
 const TripleBadgeFlow = ({
-  first, 
-  second, 
-  third
+  stepperActive,
+  steps
 }: {
-  first: string; 
-  second: string; 
-  third: string
+  stepperActive: number;
+  steps: string[]
 }) => {
   const isMobile = window.innerWidth < 768;
 
@@ -15,41 +15,35 @@ const TripleBadgeFlow = ({
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 w-full max-w-5xl h-full">
-      <div className="filter h-full drop-shadow-[0_0_1px_#000000]">
-        <div 
-          className="px-6 py-3 h-full md:py-4 flex items-center gap-2 md:gap-4 bg-white text-primary clip-path-[]"
-          style={{ clipPath: isMobile ? clipPathMobile : clipPathLeft }}
-        >
-          <div className="flex shrink-0 items-center justify-center size-8 lg:size-10 rounded-full border border-primary">
-            <span className="font-medium text-sm">01</span>
-          </div>
-          <p className="w-fit text-sm font-medium break-">{first}</p>
-        </div>
-      </div>
+      {steps.map((step, index) => {
+      let clipPath;
+      if (index === 0) {
+        clipPath = isMobile ? clipPathMobile : clipPathLeft;
+      } else if (index === 1) {
+        clipPath = isMobile ? clipPathMobile : "";
+      } else {
+        clipPath = isMobile ? clipPathMobile : clipPathRight;
+      }
 
-      <div className="filter h-full drop-shadow-[0_0_1px_#000000]">
-        <div 
-          className="px-6 py-3 h-full md:py-4 flex items-center gap-2 md:gap-4 bg-white text-[#ABB7C2]"
-          style={{ clipPath: isMobile ? clipPathMobile : "" }}
-        >
-          <div className="flex shrink-0 items-center justify-center size-8 lg:size-10 rounded-full border border-[#ABB7C2]">
-            <span className="font-medium">02</span>
+      return (
+        <div className="filter h-full drop-shadow-[0_0_1px_#000000]">
+          <div 
+            className={`${stepperActive === index ? "text-primary" : stepperActive > index ? "text-black" : "text-[#ABB7C2]"} 
+              px-6 py-3 h-full md:py-4 flex items-center gap-2 md:gap-4 bg-white`}
+            style={{ clipPath: clipPath }}
+          >
+            <div className={`${stepperActive === index ? "border-primary" : "border-[#ABB7C2]"}
+              ${stepperActive > index ? "bg-primary border-0" : "bg-transparent"}
+              flex shrink-0 items-center justify-center size-8 lg:size-10 rounded-full border`}>
+              {stepperActive > index ? 
+                <FaCheck color="#ffffff" /> :
+                <span className="font-medium text-sm">{(index + 1).toString().padStart(2, "0")}</span>
+              }
+            </div>
+            <p className="w-fit text-sm font-medium break-">{step}</p>
           </div>
-          <p className="w-fit text-sm font-medium">{second}</p>
         </div>
-      </div>
-
-      <div className="filter h-full drop-shadow-[0_0_1px_#000000]">
-        <div 
-          className="px-6 py-3 h-full md:py-4 flex items-center gap-2 md:gap-4 bg-white text-[#ABB7C2]"
-          style={{ clipPath: isMobile ? clipPathMobile : clipPathRight }}
-        >
-          <div className="flex shrink-0 items-center justify-center size-8 lg:size-10 rounded-full border border-[#ABB7C2]">
-            <span className="font-medium">03</span>
-          </div>
-          <p className="w-fit text-sm font-medium">{third}</p>
-        </div>
-      </div>
+      )})}
     </div>
   )
 }
