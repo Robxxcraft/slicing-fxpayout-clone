aebc8c55b54f1dad2abc1dad7ae0860f6a32eeac
adib <adibhaidarzaky@gmail.com>
2026-02-06 13:04:53 +0700

    add error form handle

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export function useForm<T>(initialState: T) {
   const [values, setValues] = useState<T>(initialState);
+  const [errors, setErrors] = useState<Partial<Record<keyof T, string>>>({});
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
@@ -10,9 +11,19 @@ export function useForm<T>(initialState: T) {
       ...prev,
       [name]: value,
     }));
+
+    if (errors[name as keyof T]) {
+      setErrors((prev) => ({ ...prev, [name]: undefined }));
+    }
   };
 
   const resetForm = () => setValues(initialState);
+
+  const validate = (schema: (vals: T) => Partial<Record<keyof T, string>>) => {
+    const validationsErrors = schema(values);
+    setErrors(validationsErrors);
+    return Object.keys(validationsErrors).filter((error) => error !== "").length === 0;
+  } 
 
   const setSpecificValue = (name: keyof T, value: unknown) => {
     setValues((prev) => ({ ...prev, [name]: value }));
@@ -20,9 +31,11 @@ export function useForm<T>(initialState: T) {
 
   return {
     values,
+    errors,
     handleChange,
     resetForm,
+    validate,
     setSpecificValue,
     setValues
   };
-}
+};
