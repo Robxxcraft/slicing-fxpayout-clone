9b668d60a5818ea82fe3bb02fbd5f2705e15e7cf
adib <adibhaidarzaky@gmail.com>
2026-02-06 13:11:11 +0700

    fix rebate status

--- src/components/broker/BrokerItem.tsx
+++ src/components/broker/BrokerItem.tsx
@@ -33,7 +33,7 @@ const BrokerItem = ({ item }: { item: BrokerStruc }) => {
             </h2>
           </div>
           <div className="px-2 2xl:px-4 py-3 text-sm 2xl:text-base font-medium bg-linear-to-t from-dark-primary to-primary text-white border-white h-fit rounded-full border inline-flex items-center justify-center gap-3 text-nowrap">
-            {item.statusRebate}
+            {item.statusRebate} Rebate
           </div>
         </div>
         <p className="mt-2 text-[#222222] leading-[160%]">{item.cardDescription}</p>
