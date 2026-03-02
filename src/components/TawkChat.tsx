2b9410cb07816402aaddc990e232f40ef15b2abd
adib <adibhaidarzaky@gmail.com>
2026-03-02 13:15:27 +0700

    connect to prod tawk to

--- src/components/TawkChat.tsx
+++ src/components/TawkChat.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function TawkChat() {
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = "https://embed.tawk.to/698fda893a5ba51c3b88c287/1jhcuraj0";
+    script.src = "https://embed.tawk.to/698fe853f45fd51c3bd13918/1jhd272ms";
     script.async = true;
     script.charset = "UTF-8";
     script.setAttribute("crossorigin", "*");
