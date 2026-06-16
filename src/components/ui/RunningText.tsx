e40672f084e43701983b3b29a88055f68312f425
adib <adibhaidarzaky@gmail.com>
2026-06-16 22:23:59 +0700

    change ui running text

--- src/components/ui/RunningText.tsx
+++ src/components/ui/RunningText.tsx
@@ -1,13 +1,18 @@
 import { motion } from 'framer-motion';
 import { useTranslation } from 'react-i18next';
+import MaskSvg from './MaskSvg';
+
+const detail = [
+  { icon: "/no-fees.svg", keyTranslate: "homepage:hero.runningTexts.0" },
+  { icon: "/recycle.svg", keyTranslate: "homepage:hero.runningTexts.1" },
+  { icon: "/stop.svg", keyTranslate: "homepage:hero.runningTexts.2" },
+  { icon: "/secure.svg", keyTranslate: "homepage:hero.runningTexts.3" },
+];
 
 const RunningText = ({ variant = "transparent" }: {
   variant?: "primary" | "transparent"
 }) => {
- const { t } = useTranslation(["homepage"]);
-  const runningTexts = t("homepage:hero.runningTexts", {
-    returnObjects: true
-  }) as string[];
+  const { t } = useTranslation(["homepage"]);
   
   return (
     <div className={`select-none overflow-hidden w-full whitespace-nowrap
@@ -25,18 +30,21 @@ const RunningText = ({ variant = "transparent" }: {
          >
         {Array.from({ length: 2 }).map((_, index) => (
         <div key={index} className="flex flex-nowrap items-center">
-          {runningTexts.map((text, idx) => (
+          {detail.map((item, idx) => (
             <div key={idx} 
-              className="px-3 2xl:px-4 py-4 md:py-6 flex items-center gap-3 md:gap-6 2xl:gap-10"
+              className="px-3 2xl:px-4 my-4 md:my-6 flex items-center gap-3 md:gap-6 2xl:gap-10 border-x border-[#F5F5F5]/50"
             >
-              <img src="/star.svg" alt="Icon Star" 
-                className="scale-50 md:scale-70 xl:scale-90 2xl:scale-100"
+              <MaskSvg 
+                icon={item.icon} 
+                label={"Icon"} 
+                color={"bg-white"}
+                className="size-9 2xl:size-11"              
               />
               <p className={`text-xl md:text-2xl 2xl:text-[32px] font-medium
                 ${variant === "transparent" ? "text-white":""}  
                 ${variant === "primary" ? "text-white":""}  
               `}>
-                {text}
+                {t(item.keyTranslate)}
               </p>
             </div>
           ))}
